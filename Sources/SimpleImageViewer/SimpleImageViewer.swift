15aa0855e1329898655e7a534b85c60e2cc33d39
Ilya Piatrenka <ilya.piatrenka@daftcode.pl>
2020-10-27 15:39:17 +0100

    Add proper SPM support

---
+++ Sources/SimpleImageViewer/SimpleImageViewer.swift
@@ -0,0 +1,3 @@
+struct SimpleImageViewer {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import SimpleImageViewerTests
+
+var tests = [XCTestCaseEntry]()
+tests += SimpleImageViewerTests.allTests()
+XCTMain(tests)
